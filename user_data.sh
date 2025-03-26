e785efe8b0ed71f6c045c8ac91b3425adb87dcdd
Rugved-142 <gundawar.r@northeastern.edu>
2025-03-25 21:41:44 -0400

    added parameter grp for rds and added cloudwatch config

--- user_data.sh
+++ user_data.sh
@@ -14,3 +14,6 @@
     echo "AWS_REGION=${AWS_REGION}" >> /opt/webapp/.env
     echo "AWS_S3_BUCKET_NAME=${AWS_S3_BUCKET_NAME}" >> /opt/webapp/.env
     echo "DB_HOST=${DB_HOST}" >> /opt/webapp/.env
+
+    #run cloudwatch agent
+    sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch_config.json -s
